be1ac00282d5ce668f30579f40ef91de09839dd2
Patrick McCaffrey <62127532+swinPatrick@users.noreply.github.com>
2023-05-25 19:48:51 +1000

    Update Method.cs comments

--- InferenceEngine/Methods/Method.cs
+++ InferenceEngine/Methods/Method.cs
@@ -10,7 +10,17 @@ namespace InferenceEngine
         internal List<SentenceElement> KB;
         internal List<SentenceElement> Query;
 
+        /// <summary>
+        /// Informs the inference method of known information
+        /// </summary>
+        /// <param name="aKB">The knowledge base to interpret</param>
         public abstract void Tell(List<SentenceElement> aKB);
+        
+        /// <summary>
+        /// Asks the inference method a query to assess based on what has been told.
+        /// </summary>
+        /// <param name="aKB">The query to assess</param>
+        /// <returns>String assessment of search method. what is the result of the search</returns>
         public abstract string Ask(List<SentenceElement> aQuery);
 
     }
