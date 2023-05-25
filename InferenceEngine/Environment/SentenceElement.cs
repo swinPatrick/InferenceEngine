04c98671387817f946d27b77134301693989e5c4
Patrick McCaffrey <62127532+swinPatrick@users.noreply.github.com>
2023-05-25 19:42:39 +1000

    Update SentenceElement.cs comments

--- InferenceEngine/Environment/SentenceElement.cs
+++ InferenceEngine/Environment/SentenceElement.cs
@@ -10,10 +10,11 @@ namespace InferenceEngine
 {
     public class SentenceElement
     {
-
-        public string Name { get; }
-        public Operator Operator { get; set; }
-        public int Value { get; set; }
+        public string Name { get; } // Contains either the symbol that represents the content of the node.
+        public Operator Operator { get; set; }  // defines the type of functionality held by a node.
+        public int Value { get; set; }  // value of a given node.
+        
+        // Nodes exist in a tree structure that can be navigated. with a Parent, a Left child, and a right Child.
         public SentenceElement LeftElement { get; set; }
         public SentenceElement RightElement { get; set; }
         public SentenceElement ParentElement { get; set; }
@@ -31,27 +32,15 @@ namespace InferenceEngine
                 Operator = new Itself();
         }
 
+        // Convienient functions to use the respective operator functionality
+        public bool Check() { return Operator.Check(this); }
+        public List<SentenceElement> Requires(SentenceElement aSentence = null) { return Operator.Requires(aSentence, this); }
+        public SentenceElement Apply(SentenceElement aSentenceAgenda) { return Operator.Apply(aSentenceAgenda, this); }
 
-        public bool Check()
-        {
-            return Operator.Check(this);
-        }
-        
-
-        public List<SentenceElement> Requires(SentenceElement aSentence = null)
-        {
-
-            return Operator.Requires(aSentence, this);
-        }
-
-        public SentenceElement Apply(SentenceElement aSentenceAgenda)
-        {
-            return Operator.Apply(aSentenceAgenda, this);
-        }
-
-
+        // returns a list of symbols which are below this one in the tree.
         public List<SentenceElement> GetSymbols(SentenceElement aSentence = null)
         {
+            // search itself by default.
             if (aSentence == null)
                 aSentence = this;
 
@@ -71,42 +60,10 @@ namespace InferenceEngine
             }
             return symbols;
         }
-
+        
         public override string ToString()
         {
             return (Value == 1 ? "" : "~") + Name;
         }
-
     }
-
-    /*
-    class SentenceComparer : IEqualityComparer<SentenceElement>
-    {
-        public bool Equals(SentenceElement x, SentenceElement y)
-        {
-            // sentences are equal if their name and value are equal
-            if(ReferenceEquals(x, y)) return true;
-
-            // check whether any of the compared objects is null
-            if(ReferenceEquals(x, null) || ReferenceEquals(y, null)) return false;
-
-            // check whether sentence properties are equal
-            return
-                x.Name == y.Name &&
-                x.Value == y.Value;
-        }
-
-        public int GetHashCode(SentenceElement obj)
-        {
-            // check whether the object is null
-            if(obj == null) return 0;
-
-            int hashSentenceName = obj.Name == null ? 0 : obj.Name.GetHashCode();
-
-            int hashSentenceValue = obj.Value.GetHashCode();
-
-            return hashSentenceName ^ hashSentenceValue;
-        }
-    }
-    */
 }
